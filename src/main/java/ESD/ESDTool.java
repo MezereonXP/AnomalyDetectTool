87486aa2b14e9549eb75d6309d49c3664afd9d3b
MezereonXP <mezereonxp@gmail.com>
2018-04-28 11:45:27 +0800

    add esd model

---
+++ src/main/java/ESD/ESDTool.java
@@ -0,0 +1,16 @@
+package ESD;
+
+import Tool.DetectTool;
+
+/**
+ * @program: AnomalyDetectTool
+ * @description: Extreme Studentized Deviate
+ * @author: mezereonxp Email: mezereonxp@gmail.com
+ * @create: 2018-04-28 11:41
+ **/
+public class ESDTool implements DetectTool{
+
+    public void timeSeriesAnalyse(double[] data) {
+        //TODO: 2018/4/28 实现ESD算法
+    }
+}
