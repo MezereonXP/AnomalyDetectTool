0159dd147e5662328739654f478a3e16b4b575ae
MezereonXP <mezereonxp@gmail.com>
2018-04-28 16:39:23 +0800

    add math tool

---
+++ src/main/java/Tool/MathTool.java
@@ -0,0 +1,11 @@
+package Tool;
+
+/**
+ * @program: AnomalyDetectTool
+ * @description: The tool to do some useful calculations
+ * @author: mezereonxp Email: mezereonxp@gmail.com
+ * @create: 2018-04-28 16:38
+ **/
+public class MathTool {
+    
+}
