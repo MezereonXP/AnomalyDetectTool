33879bd4ad277fad8fb63438ec5a2df858581709
MezereonXP <mezereonxp@gmail.com>
2018-05-07 18:18:07 +0800

    add S-ESD tool

---
+++ src/main/java/ESD/SESDTool.java
@@ -0,0 +1,11 @@
+package ESD;
+
+/**
+ * @program: AnomalyDetectTool
+ * @description: Seasonal Extreme Studentized Deviate
+ * @author: mezereonxp Email: mezereonxp@gmail.com
+ * @create: 2018-05-07 18:16
+ **/
+public class SESDTool {
+    
+}
