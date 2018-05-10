8dfb87353f33842c64de747c5ae1519f9ce86af3
MezereonXP <mezereonxp@gmail.com>
2018-05-10 15:20:01 +0800

    add brute force algorithm

---
+++ src/test/java/BruteForce/BruteForceToolTest.java
@@ -0,0 +1,26 @@
+package BruteForce;
+
+import Tool.DisplayTool;
+import Tool.FileTool;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+public class BruteForceToolTest {
+
+    private double[] data;
+
+    @Before
+    public void setUp() throws FileNotFoundException {
+        data = FileTool.getData("data.json");
+    }
+
+    @Test
+    public void timeSeriesAnalyse() {
+        BruteForceTool bruteForceTool = new BruteForceTool(10);
+        bruteForceTool.timeSeriesAnalyse(data);
+        DisplayTool.showResult(bruteForceTool);
+    }
+}
