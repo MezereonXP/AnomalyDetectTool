7872fc9ec516e50ad13b6e92ff9d18710bd46ece
MezereonXP <mezereonxp@gmail.com>
2018-05-16 14:36:20 +0800

    add test

---
+++ src/test/java/Tool/DisplayToolTest.java
@@ -0,0 +1,25 @@
+package Tool;
+
+import BruteForce.BruteForceTool;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+public class DisplayToolTest {
+
+    private double[] data;
+
+    @Before
+    public void setUp() throws FileNotFoundException {
+        data = FileTool.getData("data.json");
+    }
+
+    @Test
+    public void showResult() {
+        BruteForceTool bruteForceTool = new BruteForceTool(10);
+        bruteForceTool.timeSeriesAnalyse(data);
+        DisplayTool.showResult(bruteForceTool);
+    }
+}
