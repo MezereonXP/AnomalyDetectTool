a5c283724d3abb90535dc4c991184424fa47e186
MezereonXP <mezereonxp@gmail.com>
2018-06-04 11:34:44 +0800

    clean the code

--- src/main/java/IsolationTree/IsolationForest.java
+++ src/main/java/IsolationTree/IsolationForest.java
@@ -3,7 +3,7 @@ package IsolationTree;
 import Tool.MathTool;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Random;
 
 public class IsolationForest {
     private ArrayList<IsolationTree> forest;//存放的每一棵树的集合
@@ -11,57 +11,59 @@ public class IsolationForest {
     private int maxTreeHeight;//每一棵树最大的高度
     private int subSamplingNum;//随机抽样数目
     private int dataLengeth;//数据长度
-    public IsolationForest(int maxTreeNum,int maxSampling){
+
+    public IsolationForest(int maxTreeNum, int maxSampling) {
         this.subSamplingNum = maxSampling;
         this.maxTreeNum = maxTreeNum;
         this.maxTreeHeight = (int) Math.ceil(MathTool.log2(maxSampling));
-        this.forest = new ArrayList<IsolationTree>(maxTreeNum);
+        this.forest = new ArrayList<>(maxTreeNum);
     }
 
     /**
      * 创建用所给的数据创建孤立森林
+     *
      * @param data
      */
-    public void createForest(double[] data){
+    public void createForest(double[] data) {
         this.dataLengeth = data.length;
         boolean isSample = true;
-        if (data.length > this.subSamplingNum){
+        if (data.length > this.subSamplingNum) {
             isSample = false;
         }
-        for (int i = 0;i < maxTreeNum;i++){
+        for (int i = 0; i < maxTreeNum; i++) {
             IsolationTree iTree = new IsolationTree();
             if (isSample) data = subSampling(data);
-            iTree.create(data,this.maxTreeHeight);
+            iTree.create(data, this.maxTreeHeight);
             forest.add(iTree);
         }
     }
 
     /**
      * 从森林中查找数值d 的深度
+     *
      * @param d
      * @return
      */
-    public double searchForest(double d){
+    public double searchForest(double d) {
         double hx = 0;
         for (IsolationTree tree : this.forest) {
             hx += tree.pathLength(d);
         }
-        double s = (double)this.maxTreeNum;
-        double cn = 2 * (Math.log(this.dataLengeth - 1)+ MathTool.EulerConstant) - 2 * (this.dataLengeth - 1) / this.dataLengeth;
-        double score = Math.pow(2,-hx/this.maxTreeNum/cn);
-        return score;
+        double cn = 2 * (Math.log(this.dataLengeth - 1.0) + MathTool.EulerConstant) - 2 * (this.dataLengeth - 1) / this.dataLengeth;
+        return Math.pow(2, -hx / this.maxTreeNum / cn);
     }
 
     /**
      * 对数据进行随机抽样
+     *
      * @param data
      * @return
      */
     private double[] subSampling(double[] data) {
         int n = data.length;
         double[] sample = new double[this.subSamplingNum];
-        for (int i = 0;i < this.subSamplingNum;i++){
-            int r = (int) (Math.random() * n);
+        for (int i = 0; i < this.subSamplingNum; i++) {
+            int r = new Random().nextInt() * n;
             sample[i] = data[r];
             data[r] = data[--n];
         }
--- src/main/java/IsolationTree/IsolationTreeTool.java
+++ src/main/java/IsolationTree/IsolationTreeTool.java
@@ -7,33 +7,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class IsolationTreeTool implements DetectTool {
+
+    private int maxTreeNum = 100;
+    private int maxSampling = 256;
+
     private ArrayList<Result> results;
-    private int subSamplingNumber;
-    private int treeNumber;
     private IsolationForest forest;
+
+    public IsolationTreeTool(){}
+
+    public IsolationTreeTool(int maxTreeNum, int maxSampling) {
+        this.maxTreeNum = maxTreeNum;
+        this.maxSampling = maxSampling;
+    }
+
     @Override
     public void timeSeriesAnalyse(double[] data) {
-        this.results = new ArrayList<Result>();
-        IsolationForest forest = new IsolationForest(100,256);
-        forest.createForest(data.clone());
-        this.forest = forest;
-        cutAnomaly(data,0.6f);
+        this.results = new ArrayList<>();
+        IsolationForest tempForest = new IsolationForest(maxTreeNum, maxSampling);
+        tempForest.createForest(data.clone());
+        this.forest = tempForest;
+        cutAnomaly(data, 0.6f);
     }
 
     /**
      * 选出所有异常值
+     *
      * @param data
      * @param factor
      */
-    public void cutAnomaly(double[] data,float factor){
-        for (int i = 0;i < data.length;i++){
-            if (this.forest.searchForest(data[i]) > factor){
-                this.results.add(new Result(i,data[i]));
+    public void cutAnomaly(double[] data, float factor) {
+        for (int i = 0; i < data.length; i++) {
+            if (this.forest.searchForest(data[i]) > factor) {
+                this.results.add(new Result(i, data[i]));
             }
         }
     }
+
     @Override
     public List<Result> getResults() {
         return results;
+    }
+
+    public int getMaxSampling() {
+        return maxSampling;
+    }
+
+    public void setMaxSampling(int maxSampling) {
+        this.maxSampling = maxSampling;
+    }
+
+    public int getMaxTreeNum() {
+        return maxTreeNum;
+    }
+
+    public void setMaxTreeNum(int maxTreeNum) {
+        this.maxTreeNum = maxTreeNum;
     }
 }
