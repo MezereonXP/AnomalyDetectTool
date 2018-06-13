5ed6d405ec074eb0f16d2f00fdb3414ca870d550
MezereonXP <mezereonxp@gmail.com>
2018-06-13 17:04:01 +0800

    [UPDATE] Add some test

---
+++ src/test/java/IsolationTree/IsolationForestTest.java
@@ -0,0 +1,11 @@
+package IsolationTree;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class IsolationForestTest {
+
+    @Test
+    public void createForest() {
+    }
+}
---
+++ src/test/java/IsolationTree/IsolationTreeNodeTest.java
@@ -0,0 +1,7 @@
+package IsolationTree;
+
+import static org.junit.Assert.*;
+
+public class IsolationTreeNodeTest {
+
+}
